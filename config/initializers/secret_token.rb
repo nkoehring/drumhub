fde514b913f98c810a1b48b186c48dd5f5720306
Norman Köhring <n@koehr.in>
2013-03-31 22:34:44 +0200

    initial empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drumhub::Application.config.secret_token = '99e439c90d1adff71aff086a2c1dba58c38ac160cef26008e3a906f40977dfd797925ebe09cbb9783018a92a8a3c21c52bdbb4af7a95d57b205d86485eca3c0c'
